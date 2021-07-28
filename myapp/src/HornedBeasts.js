8bfaac3debb538a277552d9203afcaba18dd010b
duaa-tarazi <dst08itm@yahoo.com>
2021-07-28 16:15:31 +0300

    add cards to the page

--- myapp/src/HornedBeasts.js
+++ myapp/src/HornedBeasts.js
@@ -1,16 +1,38 @@
 import React from "react";
+import Card from 'react-bootstrap/Card';
+
+
 
 class  HornedBeasts extends React.Component{
+    constructor(props){
+        super (props);
+        this.state={
+            numOfClicks:0
+        }
+
+    }
+    incrementNumofLikes=()=>{
+this.setState({
+    numOfClicks:this.state.numOfClicks + 1
+})
+    }
     render(){
         return(
             <>
-               <h2>{this.props.title}</h2>
-               <img
-               src={this.props.imageUrl}
-               alt={this.props.title}
-               title={this.props.title}
-               />
-               <p>{this.props.description}</p>
+
+              
+
+               <Card style={{ width: '18rem' }}>
+  <Card.Img onClick={this.incrementNumofLikes} variant="top" src={this.props.imageUrl} />
+  <Card.Body>
+    <Card.Title>{this.props.title}</Card.Title>
+    <Card.Text>
+    {this.props.description}
+    </Card.Text>
+   <p>Num of clicks:
+       {this.state.numOfClicks}</p>
+  </Card.Body>
+</Card>
             </> 
                );
     }
