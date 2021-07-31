56360dbba7b94348023982753775effedf037def
duaa-tarazi <dst08itm@yahoo.com>
2021-08-01 02:30:04 +0300

    add modal

--- myapp/src/HornedBeasts.js
+++ myapp/src/HornedBeasts.js
@@ -17,12 +17,14 @@ class HornedBeasts extends React.Component {
             numOfClicks: this.state.numOfClicks + 1
         })
     }
+    getSelectedData=() =>{
+        this.props.handleopen();
+        this.props.getSelectedData(this.props.imageUrl,this.props.title,this.props.description,this.state.numOfClicks)
+    };
     render() {
         return (
-
             <Col>
-
-                <Card style={{ width: '18rem' }}>
+                <Card style={{ width: '18rem' }} onClick={this.getSelectedData}>
                     <Card.Img onClick={this.incrementNumofLikes} variant="top" src={this.props.imageUrl} />
                     <Card.Body>
                         <Card.Title>{this.props.title}</Card.Title>
@@ -34,10 +36,7 @@ class HornedBeasts extends React.Component {
                     </Card.Body>
                 </Card>
             </Col>
-
-
         );
     }
-
 }
 export default HornedBeasts;
