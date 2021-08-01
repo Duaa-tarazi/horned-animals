11e836156433b5484c6dfc46d931af6ce337f3ec
duaa-tarazi <dst08itm@yahoo.com>
2021-08-01 11:26:28 +0300

    add select form

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -5,54 +5,75 @@ import Footer from './Footer';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Row from 'react-bootstrap/esm/Row';
 import datajson from './data.json';
-import selectedBeast from './selectedBeast'
+import selectedBeast from './selectedBeast';
+import Form from 'react-bootstrap/esm/Form';
+import { data } from 'jquery';
 
-class App extends React.Component{
-  constructor(props){
+class App extends React.Component {
+  constructor(props) {
     super(props);
-    this.state={
-      show:false,
-      title:'',
-      src:'',
-      description:'',
+    this.state = {
+      show: false,
+      title: '',
+      src: '',
+      description: '',
+      filteredDatajson: datajson
     }
   }
-  handleOpen= () =>{
+  handleOpen = () => {
     this.setState({
-      show:true
+      show: true
     })
   }
-  handleClose=()=>{
+  handleClose = () => {
     this.setState({
-      show:false
+      show: false
     })
-  getSelectedBeastData=(title,src,description) =>{
+    getSelectedBeastData = (title, src, description) => {
       this.setState({
-        title:title,
-        src:src,
-        description:description
+        title: title,
+        src: src,
+        description: description
       })
+    }
   }
+  getNumOfHorns = (event) => {
+    let horns = event.target.value;
+    let filterData1 = data.filter((item) => {
+      if (item.horns == horns)
+      
+    this.setState({
+      filteredDatajson: filterData1
+    });
   }
-  render(){
-    return(
+
+  render() {
+    return (
       <>
-      <Header/>
-      <Row xs={1} md={3} className="g-4">
-      <Main
-      datajson={datajson}
-      handleOpen={this.handleOpen}
-      getSelectedBeastData={this.getSelectedBeastData}
-      />
-      </Row>
-      <selectedBeast
-      show={this.state.show}
-      handleClose={this.handleClose}
-      title={this.state.title}
-      src={this.state.src}
-      description={this.state.description}
-      />
-      <Footer/>
+        <Header />
+        <Form.Lable>HOW MANY HOURS?</Form.Lable>
+        <Form.Select aria-label="Default select example" onChange={this.getNumOfHorns}>
+          <option value="all">All</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+          <option value="Wooow">Woow</option>
+        </Form.Select>
+        <Row xs={1} md={3} className="g-4">
+          <Main
+            datajson={this.state.filterData}
+            handleOpen={this.handleOpen}
+            getSelectedBeastData={this.getSelectedBeastData}
+          />
+        </Row>
+        <selectedBeast
+          show={this.state.show}
+          handleClose={this.handleClose}
+          title={this.state.title}
+          src={this.state.src}
+          description={this.state.description}
+        />
+        <Footer />
       </>
     )
   }
